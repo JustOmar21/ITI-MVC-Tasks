0305767648e4ed1e0f21fa0197a2f2bebdb36d0b
Omar <tarekes68@gmail.com>
2025-03-09 12:56:26 +0200

    Add project files.

---
+++ Day 02/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day_02.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string Name, string Picture, double Price, string Description)
+        {
+            ViewBag.Name = Name;
+            ViewBag.Picture = Picture;
+            ViewBag.Price = Price;
+            ViewBag.Description = Description;
+            return View();
+        }
+    }
+}
