331b92152a171d1c5892c3694b0d331d871d3769
Omar <tarekes68@gmail.com>
2025-03-11 13:11:33 +0200

    added Day 4

---
+++ Day 04/Controllers/CarController.cs
@@ -0,0 +1,55 @@
+﻿using Day_03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day_03.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(CarList.Cars);
+        }
+
+        [Route("/Car/{id:int}")]
+        public IActionResult Details(int id)
+        {
+            return View(CarList.Cars.SingleOrDefault(car => car.ID == id));
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Car car)
+        {
+            CarList.Cars.Add(car);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View(CarList.Cars.SingleOrDefault(car => car.ID == id));
+        }
+
+        [HttpPost]
+        public IActionResult Update(Car car)
+        {
+            var updatedCar = CarList.Cars.SingleOrDefault(ca => ca.ID == car.ID);
+            updatedCar.Model = car.Model;
+            updatedCar.Color = car.Color;
+            updatedCar.Manfacture = car.Manfacture;
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var car = CarList.Cars.SingleOrDefault(c => c.ID == id);
+            CarList.Cars.Remove(car);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Day 04/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Day_03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day_03.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(User user)
+        {
+            ViewBag.User = user;
+            return View();
+        }
+    }
+}
